23fa6f103f56568842cd2616a20779e25c846a1c
JoseAcevedo6 <acevedo0803@gmail.com>
2022-12-15 23:33:26 -0300

    Updated with DecisionTreeClassifier

--- src/model.py
+++ src/model.py
@@ -1,22 +1,30 @@
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from pandas import DataFrame, read_csv
 
-from etl import transform
+from transform import transform
 
 
 data = read_csv('./datasets/hospitalizaciones_train.csv')
-columns = ['Department', 'Ward_Facility_Code', 'doctor_name', 'Age', 'gender', 'Stay (in days)']
-X_Train, X_Test, Y_Train, Y_Test = transform(
-        data, columns, target='Stay (in days)', test_size=0.20, random_state=42, array=True, scalar=True)
 
-knn_model = KNeighborsClassifier(n_neighbors=7)
-knn_model.fit(X_Train, Y_Train)
-Y_Pred = knn_model.predict(X_Test)
+le = [
+        'Department', 'Ward_Facility_Code', 'doctor_name', 'Age', 'gender',
+        'Type of Admission', 'Severity of Illness', 'health_conditions', 'Insurance']
+
+ss = ['Available Extra Rooms in Hospital', 'staff_available', 'Visitors with Patient', 'Admission_Deposit']
+
+target = ['Stay (in days)']
+
+X_Train, X_Test, Y_Train, Y_Test = transform(
+        data, to_le=le, to_ss=ss, to_tts=target, ros=False, test_size=0.2, random_state=42, array=False)
+
+dtc_model = DecisionTreeClassifier(max_depth=13, criterion='entropy', random_state=42)
+dtc_model.fit(X_Train, Y_Train)
+Y_Pred = dtc_model.predict(X_Test)
+
 
 data2 = read_csv('./datasets/hospitalizaciones_test.csv')
-columns_data2 = ['Department', 'Ward_Facility_Code', 'doctor_name', 'Age', 'gender']
-eval = transform(data2, columns_data2, array=True, scalar=True)
 
-predition = DataFrame(knn_model.predict(eval))
-predition.columns = ['pred']
+eval = transform(data2, to_le=le, to_ss=ss)
+
+predition = DataFrame(dtc_model.predict(eval), columns=['pred'])
 predition.to_csv('JoseAcevedo6', index=False)
